e09daabc91a0aec9e7d80ef291aa11d2fa07c041
Xavopls <xavier.olivares01@estudiant.upf.edu>
2016-11-23 12:10:42 +0100

    Clients fotuts

---
+++ Project_DAD/src/pkg_1/Clients.java
@@ -0,0 +1,36 @@
+package pkg_1;
+
+import java.util.List;
+
+public class Clients {
+	private List<Client> clients;
+
+	@Override
+	public String toString() {
+		return "Clients [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+
+	public List<Client> getClients() {
+		return clients;
+	}
+
+	public void setClients(List<Client> clients) {
+		this.clients = clients;
+	}
+
+	public void insertClient(Client client) {
+		if (clients.contains(client))
+			clients.add(client);
+		else
+			System.out.println("The client already exists!");
+	}
+
+	public void deleteClient(Client client) {
+		if (clients.contains(client))
+			clients.remove(client);
+		else
+			System.out.println("Unable to delete, user does not exist!");
+	}
+
+}
